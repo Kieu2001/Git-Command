2f43d686a988411299e72eeefd14f8b0e597d6d3
Tuan Anh <tuananh462001@gmail.com>
2024-03-07 22:07:49 +0700

    Checkadd

--- Git-Command/Git-Command/Controllers/HomeController.cs
+++ Git-Command/Git-Command/Controllers/HomeController.cs
@@ -16,6 +16,13 @@ namespace Git_Command.Controllers
             +Nếu muốn chọn tập cụ thể thì sử dụng lênh: git add 
          - Git fetch:
          - Git amend:
+           Nếu muốn sửa đổi commit cũ mà không muốn tọa 1 commit mới thì sử dụng lệnh:
+           git add . -> git commit --amend -> git push --fetch origin <Branch name>
+         - Git checkout:
+           Nếu muốn chuyển từ nhánh A sang nhánh B thì dùng câu lệnh: git checkout B
+           Note: Trong trường hợp nhánh A đang có sự thay đổi mà chưa muốn commit
+                 thì sử dụng git stash để lưu lại những thay đổi: git stash
+                 Muốn lấy lại những thay đổi thì sử dụng lệnh: git
          
          */
     }
