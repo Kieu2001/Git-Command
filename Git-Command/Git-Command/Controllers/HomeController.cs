e3f8496db9405ba5ef1067b99abd3f97a4d51b64
Tuan Anh <tuananh462001@gmail.com>
2024-03-07 21:40:17 +0700

    Check

---
+++ Git-Command/Git-Command/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git_Command.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+         - Git commit:
+         
+         - Git fetch:
+         - Git amend:
+         
+         */
+    }
+}
